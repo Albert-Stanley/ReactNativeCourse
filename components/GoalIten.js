1a7776a3d129ef6d2a6177f8d011670002304836
Albert Stanley <153841479+Albert-Stanley@users.noreply.github.com>
2024-12-12 15:58:15 -0300

    working with pressable

--- components/GoalIten.js
+++ components/GoalIten.js
@@ -1,24 +1,33 @@
-import { StyleSheet, View, Text } from "react-native";
+import { StyleSheet, View, Text, Pressable } from "react-native";
 
 function GoalItem(props) {
   return (
     <View style={styles.goalsList}>
-      <Text style={styles.goalText}>{props.text}</Text>
+      <Pressable
+        android_ripple={{ color: "#ffffff" }}
+        onPress={props.onDeleteItem.bind(this, props.id)}
+        style={({ pressed }) => (pressed ? styles.pressItem : null)}
+      >
+        <Text style={styles.goalText}>{props.text}</Text>
+      </Pressable>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   goalsList: {
-    padding: 8,
     fontSize: 15,
     marginBottom: 10,
     borderRadius: 10,
     borderWidth: 2,
     backgroundColor: "#34495e",
   },
+  pressItem: {
+    opacity: 0.5,
+  },
   goalText: {
     color: "white",
+    padding: 8,
   },
 });
 
